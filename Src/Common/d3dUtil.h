7cd6923cf15341a0f9e21fb9b76283028183e7b5
Carlos López Garcés <carlos.lopez.garces@gmail.com>
2021-07-07 10:10:08 -0700

    Add MeshGeometry and SubmeshGeometry

--- Src/Common/d3dUtil.h
+++ Src/Common/d3dUtil.h
@@ -65,3 +65,67 @@ public:
     Microsoft::WRL::ComPtr<ID3D12Resource> &uploadBuffer
   );
 };
+
+// A component mesh of a MeshGeometry. The vertices and indices of a SubmeshGeometry
+// are contained in the same buffers as other components of a MeshGeometry. The
+// vertices and indices of a given SubmeshGeometry are stored contiguously in the buffers.
+struct SubmeshGeometry {
+  UINT IndexCount = 0;
+  UINT StartIndexLocation = 0;
+  INT BaseVertexLocation = 0;
+  DirectX::BoundingBox Bounds;
+};
+
+// Groups a vertex and index buffer together. May be made of component SubmeshGeometry's.
+struct MeshGeometry {
+  std::string Name;
+
+  // The stride or pitch is the byte size of an element in the buffer.
+  UINT VertexByteStride = 0;
+  // Total size.
+  UINT VertexBufferByteSize = 0;
+
+  DXGI_FORMAT IndexFormat = DXGI_FORMAT_R16_UINT;
+  // Total size.
+  UINT IndexBufferByteSize = 0;
+
+  // This is where the vertex and index buffers are kept on the CPU side.
+  Microsoft::WRL::ComPtr<ID3DBlob> VertexBufferCPU = nullptr;
+  Microsoft::WRL::ComPtr<ID3DBlob> IndexBufferCPU = nullptr;
+
+  Microsoft::WRL::ComPtr<ID3D12Resource> VertexBufferGPU = nullptr;
+  Microsoft::WRL::ComPtr<ID3D12Resource> IndexBufferGPU = nullptr;
+
+  // The vertex and index buffer resources will go in default buffers, so we
+  // need upload buffers to copy their data into them (resources in the default
+  // heap can only be accessed by the CPU via intermediate upload buffers).
+  Microsoft::WRL::ComPtr<ID3D12Resource> VertexBufferUploader = nullptr;
+  Microsoft::WRL::ComPtr<ID3D12Resource> IndexBufferUploader = nullptr;
+
+  // Component SubmeshGeometry's. The vertices and indices of component 
+  // SubmeshGeometry's coexist in the same vertex and index buffers.
+  std::unordered_map<std::string, SubmeshGeometry> DrawArgs;
+
+  // Vertex buffer resource descriptor.
+  D3D12_VERTEX_BUFFER_VIEW VertexBufferView() const {
+    D3D12_VERTEX_BUFFER_VIEW vbv;
+    vbv.BufferLocation = VertexBufferGPU->GetGPUVirtualAddress();
+    vbv.StrideInBytes = VertexByteStride;
+    vbv.SizeInBytes = VertexBufferByteSize;
+    return vbv;
+  }
+
+  // Index buffer resource descriptor.
+  D3D12_INDEX_BUFFER_VIEW IndexBufferView() const {
+    D3D12_INDEX_BUFFER_VIEW ibv;
+    ibv.BufferLocation = IndexBufferGPU->GetGPUVirtualAddress();
+    ibv.Format = IndexFormat;
+    ibv.SizeInBytes = IndexBufferByteSize;
+    return ibv;
+  }
+
+  void DisposeUploaders() {
+    VertexBufferUploader = nullptr;
+    IndexBufferUploader = nullptr;
+  }
+};
