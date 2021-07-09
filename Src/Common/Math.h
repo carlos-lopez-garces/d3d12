73db9d264094942faf6d46e92d7f0aeb34ade78d
Carlos López Garcés <carlos.lopez.garces@gmail.com>
2021-07-08 17:55:29 -0700

    Create Src/Common/Math and add Identity4x4

---
+++ Src/Common/Math.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <Windows.h>
+#include <DirectXMath.h>
+#include <cstdint>
+
+DirectX::XMFLOAT4X4 Identity4x4() {
+	static DirectX::XMFLOAT4X4 I(
+		1.0f, 0.0f, 0.0f, 0.0f,
+		0.0f, 1.0f, 0.0f, 0.0f,
+		0.0f, 0.0f, 1.0f, 0.0f,
+		0.0f, 0.0f, 0.0f, 1.0f
+	);
+
+	return I;
+}
