60a6953803fc44a6ca7b13b5dc3041aeef0fb5e5
Carlos López Garcés <carlos.lopez.garces@gmail.com>
2021-07-08 18:22:04 -0700

    Put math functions in Math namespace

--- Src/Common/Math.h
+++ Src/Common/Math.h
@@ -4,13 +4,17 @@
 #include <DirectXMath.h>
 #include <cstdint>
 
-DirectX::XMFLOAT4X4 Identity4x4() {
-	static DirectX::XMFLOAT4X4 I(
-		1.0f, 0.0f, 0.0f, 0.0f,
-		0.0f, 1.0f, 0.0f, 0.0f,
-		0.0f, 0.0f, 1.0f, 0.0f,
-		0.0f, 0.0f, 0.0f, 1.0f
-	);
+namespace Math {
 
-	return I;
+	DirectX::XMFLOAT4X4 Identity4x4() {
+		static DirectX::XMFLOAT4X4 I(
+			1.0f, 0.0f, 0.0f, 0.0f,
+			0.0f, 1.0f, 0.0f, 0.0f,
+			0.0f, 0.0f, 1.0f, 0.0f,
+			0.0f, 0.0f, 0.0f, 1.0f
+		);
+
+		return I;
+	}
+
 }
