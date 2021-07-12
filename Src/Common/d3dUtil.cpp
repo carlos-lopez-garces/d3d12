e603f4d49daa34c7415385eb5033fe1390a0a404
Carlos López Garcés <carlos.lopez.garces@gmail.com>
2021-07-11 20:56:27 -0700

    Add CompileShader

--- Src/Common/d3dUtil.cpp
+++ Src/Common/d3dUtil.cpp
@@ -79,3 +79,38 @@ Microsoft::WRL::ComPtr<ID3D12Resource> d3dUtil::CreateDefaultBuffer(
 
   return defaultBuffer;
 }
+
+Microsoft::WRL::ComPtr<ID3DBlob> d3dUtil::CompileShader(
+  const std::wstring& filename,
+  const D3D_SHADER_MACRO* defines,
+  const std::string& entrypoint,
+  const std::string& target
+) {
+  UINT compileFlags = 0;
+#if defined(DEBUG) || defined(_DEBUG)  
+  compileFlags = D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION;
+#endif
+
+  HRESULT hr = S_OK;
+
+  ComPtr<ID3DBlob> byteCode = nullptr;
+  ComPtr<ID3DBlob> errors;
+  hr = D3DCompileFromFile(
+    filename.c_str(),
+    defines,
+    D3D_COMPILE_STANDARD_FILE_INCLUDE,
+    entrypoint.c_str(),
+    target.c_str(),
+    compileFlags,
+    0,
+    &byteCode,
+  &errors);
+
+  if (errors != nullptr) {
+    OutputDebugStringA((char*)errors->GetBufferPointer());
+  }
+
+  ThrowIfFailed(hr);
+
+  return byteCode;
+}
--- Src/Common/d3dUtil.h
+++ Src/Common/d3dUtil.h
@@ -69,6 +69,13 @@ public:
   static UINT CalcConstantBufferByteSize(UINT byteSize) {
     return (byteSize + 255) & ~255;
   }
+
+  static Microsoft::WRL::ComPtr<ID3DBlob> CompileShader(
+    const std::wstring &filename,
+    const D3D_SHADER_MACRO *defines,
+    const std::string &entrypoint,
+    const std::string &target
+  );
 };
 
 // A component mesh of a MeshGeometry. The vertices and indices of a SubmeshGeometry
