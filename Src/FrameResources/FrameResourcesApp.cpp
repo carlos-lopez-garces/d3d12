579bf322776819951deb831d1e5ec57612fabd60
Carlos López Garcés <carlos.lopez.garces@gmail.com>
2021-07-22 14:16:17 -0700

    Add FrameResourcesApp::UpdateMainPassCB

--- Src/FrameResources/FrameResourcesApp.cpp
+++ Src/FrameResources/FrameResourcesApp.cpp
@@ -34,9 +34,18 @@ class FrameResourcesApp : public D3DApp {
 private:
   FrameResource* mCurrFrameResource = nullptr;
   std::vector<std::unique_ptr<RenderItem>> mAllRenderItems;
+  XMFLOAT4X4 mView = Math::Identity4x4();
+  XMFLOAT4X4 mProj = Math::Identity4x4();
+  XMFLOAT3 mEyePos = { 0.0f, 0.0f, 0.0f };
+  // Data that applies to all draw calls and that doesn't depend on the object
+  // being drawn.
+  PassConstants mMainPassCB;
 
 private:
   void UpdateObjectCBs(const GameTimer &gt);
+  // Updates the pass constants (e.g. view and projection matrices) and sets
+  // them in the current frame resource.
+  void UpdateMainPassCB(const GameTimer &gt);
 };
 
 void FrameResourcesApp::UpdateObjectCBs(const GameTimer& gt) {
@@ -54,4 +63,35 @@ void FrameResourcesApp::UpdateObjectCBs(const GameTimer& gt) {
       e->NumFramesDirty--;
     }
   }
+}
+
+void FrameResourcesApp::UpdateMainPassCB(const GameTimer& gt) {
+  XMMATRIX view = XMLoadFloat4x4(&mView);
+  XMMATRIX proj = XMLoadFloat4x4(&mProj);
+
+  XMMATRIX viewProj = XMMatrixMultiply(view, proj);
+  // TODO: does XMMatrixInverse return the determinant of the given matrix in the
+  // first pointer input? Or does it receive it? It looks like XMMatrixDeterminant
+  // is the one that computes it, not XMMatrixInverse. Either way, the first argument
+  // is NOT optional.
+  XMMATRIX invView = XMMatrixInverse(&XMMatrixDeterminant(view), view);
+  XMMATRIX invProj = XMMatrixInverse(&XMMatrixDeterminant(proj), proj);
+  XMMATRIX invViewProj = XMMatrixInverse(&XMMatrixDeterminant(viewProj), viewProj);
+
+  XMStoreFloat4x4(&mMainPassCB.View, XMMatrixTranspose(view));
+  XMStoreFloat4x4(&mMainPassCB.InvView, XMMatrixTranspose(invView));
+  XMStoreFloat4x4(&mMainPassCB.Proj, XMMatrixTranspose(proj));
+  XMStoreFloat4x4(&mMainPassCB.InvProj, XMMatrixTranspose(invProj));
+  XMStoreFloat4x4(&mMainPassCB.ViewProj, XMMatrixTranspose(viewProj));
+  XMStoreFloat4x4(&mMainPassCB.InvViewProj, XMMatrixTranspose(invViewProj));
+  mMainPassCB.EyePosW = mEyePos;
+  mMainPassCB.RenderTargetSize = XMFLOAT2((float) mClientWidth, (float) mClientHeight);
+  mMainPassCB.InvRenderTargetSize = XMFLOAT2(1.0f / mClientWidth, 1.0f / mClientHeight);
+  mMainPassCB.NearZ = 1.0f;
+  mMainPassCB.FarZ = 1.0f;
+  mMainPassCB.TotalTime = gt.TotalTime();
+  mMainPassCB.DeltaTime = gt.DeltaTime();
+
+  auto currPassCB = mCurrFrameResource->PassCB.get();
+  currPassCB->CopyData(0, mMainPassCB);
 }
