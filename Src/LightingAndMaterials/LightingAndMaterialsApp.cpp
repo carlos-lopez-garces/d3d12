ff0b3dde367a6d2ad36dcfb12fadbcba456a89eb
Carlos López Garcés <carlos.lopez.garces@gmail.com>
2021-08-17 12:04:37 -0700

    Add LightingAndMaterialsApp::UpdateMaterialCBs,DrawRenderItems

--- Src/LightingAndMaterials/LightingAndMaterialsApp.cpp
+++ Src/LightingAndMaterials/LightingAndMaterialsApp.cpp
@@ -1,14 +1,45 @@
 #include <DirectXMath.h>
 #include "../Common/d3dApp.h"
+#include "../Common/Math.h"
+#include "../Common/d3dUtil.h"
+#include "FrameResource.h"
+
+const int gNumFrameResources = 3;
+
+struct RenderItem {
+  XMFLOAT4X4 World = Math::Identity4x4();
+
+  int NumFramesDirty = gNumFrameResources;
+
+  // Index into the object constant buffer where this render item's data is passed to the pipeline.
+  UINT ObjCBIndex = -1;
+
+  // A pointer because multople render items may use the same material.
+  Material* Mat = nullptr;
+  
+  MeshGeometry* Geo = nullptr;
+
+  // The D3D12 version of TRIANGLELIST doesn't seem to exist.
+  D3D12_PRIMITIVE_TOPOLOGY PrimitiveType = D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+
+  UINT IndexCount = 0;
+  UINT StartIndexLocation = 0;
+  int BaseVertexLocation = 0;
+};
 
 class LightingAndMaterialsApp : D3DApp {
 private:
   std::unordered_map<std::string, std::unique_ptr<Material>> mMaterials;
+  std::vector<std::unique_ptr<FrameResource>> mFrameResources;
+  FrameResource *mCurrFrameResource = nullptr;
+  int mCurrFrameResourceIndex = 0;
 
 public:
 
 private:
   void BuildMaterials();
+  void UpdateMaterialCBs(const GameTimer &gt);
+  void DrawRenderItems(ID3D12GraphicsCommandList *cmdList, const std::vector<RenderItem *> &ritems);
 };
 
 void LightingAndMaterialsApp::BuildMaterials() {
@@ -30,4 +61,46 @@ void LightingAndMaterialsApp::BuildMaterials() {
 
   mMaterials["grass"] = std::move(grass);
   mMaterials["water"] = std::move(water);
+}
+
+void LightingAndMaterialsApp::UpdateMaterialCBs(const GameTimer& gt) {
+  auto currMaterialCB = mCurrFrameResource->MaterialCB.get();
+  for (auto& e : mMaterials) {
+    Material* mat = e.second.get();
+    if (mat->NumFramesDirty > 0) {
+      // The application changed the material. Update the copies stored in constant
+      // buffers of each of the frame resources.
+      MaterialContants matConstants;
+      matConstants.DiffuseAlbedo = mat->DiffuseAlbedo;
+      matConstants.FresnelR0 = mat->FresnelR0;
+      matConstants.Roughness = mat->Roughness;
+
+      currMaterialCB->CopyData(mat->MatCBIndex, matConstants);
+      mat->NumFramesDirty--;
+    }
+  }
+}
+
+void LightingAndMaterialsApp::DrawRenderItems(ID3D12GraphicsCommandList* cmdList, const std::vector<RenderItem*>& ritems) {
+  UINT objCBByteSize = d3dUtil::CalcConstantBufferByteSize(sizeof(ObjectConstants));
+  UINT matCBByteSize = d3dUtil::CalcConstantBufferByteSize(sizeof(MaterialConstants));
+
+  auto objectCB = mCurrFrameResource->ObjectCB->Resource();
+  auto matCB = mCurrFrameResource->MaterialCB->Resource();
+
+  for (size_t i = 0; i < ritems.size(); ++i) {
+    auto ri = ritems[i];
+    cmdList->IASetVertexBuffers(0, 1, &ri->Geo->VertexBufferView());
+    cmdList->IASetIndexBuffer(&ri->Geo->IndexBufferView());
+    cmdList->IASetPrimitiveTopology(ri->PrimitiveType);
+
+    D3D12_GPU_VIRTUAL_ADDRESS objCBAddress = objectCB->GetGPUVirtualAddress() + ri->ObjCBIndex * objCBByteSize;
+    // The constant buffer index where the material goes is not stored directly in the
+    // RenderItem because it may be shared among many.
+    D3D12_GPU_VIRTUAL_ADDRESS matCBAddress = matCB->GetGPUVirtualAddress() + ri->Mat->MatCBIndex * matCBByteSize;
+  
+    cmdList->SetGraphicsRootConstantBufferView(0, objCBAddress);
+    cmdList->SetGraphicsRootConstantBufferView(1, matCBAddress);
+    cmdList->DrawIndexedInstanced(ri->IndexCount, 1, ri->StartIndexLocation, ri->BaseVertexLocation, 0);
+  }
 }
