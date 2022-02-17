1d85827fdce045be8cab6c06baffd1883263d292
Carlos López Garcés <carlos.lopez.garces@gmail.com>
2022-02-16 23:30:10 -0800

    Add ShadowMapping.hlsl

---
+++ Src/ShadowMapping/ShadowMapping.hlsl
@@ -0,0 +1,90 @@
+#ifndef NUM_DIR_LIGHTS
+    #define NUM_DIR_LIGHTS 3
+#endif
+
+#ifndef NUM_POINT_LIGHTS
+    #define NUM_POINT_LIGHTS 0
+#endif
+
+#ifndef NUM_SPOT_LIGHTS
+    #define NUM_SPOT_LIGHTS 0
+#endif
+
+#include "Common.hlsl"
+
+struct VertexIn {
+    // Local coordinates.
+    float3 PosL : POSITION;
+    float3 NormalL : NORMAL;
+    float2 TexC : TEXCOORD;
+    float3 TangentU : TANGENT;
+};
+
+struct VertexOut {
+    float4 PosH : SV_POSITION;
+    float4 ShadowPosH : POSITION0;
+    float3 PosW : POSITION1;
+    float3 NormalW : NORMAL;
+    float3 TangentW : TANGENT;
+    float2 TexC : TEXCOORD;
+};
+
+VertexOut VS(VertexIn vin) {
+    VertexOut vout = (VertexOut) 0.0f;
+    MaterialData matData = gMaterialData[gMaterialIndex];
+
+    float4 posW = mul(float4(vin.PosL, 1.0f), gWorld);
+    vout.PosW = posW.xyz;
+    vout.NormalW = mul(vin.NormalL, (float3x3) gWorld);
+    vout.TangentW = mul(vin.TangentW, (float3x3) gWorld);
+    vout.PosH = mul(posW, gViewProj);
+
+    float4 texC = mul(float4(vin.TexC, 0.0f, 1.0f), gTexTransform);
+    vout.TexC = mul(texC, matData.MatTransform).xy;
+    
+    vout.ShadowPosH = mul(posW, gShadowTransform);
+
+    return vout;
+}
+
+float4 PS(VertexOut pin) : SV_TARGET {
+    MaterialData matData = gMaterialData[gMaterialIndex];
+    float4 diffuseAlbedo = matData.DiffuseAlbedo;
+    float3 fresnelR0 = matData.FresnelR0;
+    float roughness = matData.Roughness;
+    uint diffuseMapIndex = matData.DiffuseMapIndex;
+    uint normalMapIndex = matData.NormalMapIndex;
+
+    diffuseAlbedo *= gTextureMaps[diffuseMapIndex].Sample(gsamAnisotropicWrap, pin.TexC);
+
+#ifdef ALPHA_TEST
+    clip(diffuseAlbedo.a - 0.1f);
+#endif
+
+    pin.NormalW = normalize(pin.NormalW);
+    float4 normalMapSample = gTextureMaps[normalMapIndex].Sample(gsamAnisotropicWrap, pin.TexC);
+    float3 bumpedNormalW = NormalSampleToWorldSpace(normalMapSample.rgb, pin.NormalW, pin.TangentW);
+
+    float3 toEyeW = normalize(gEyePosW - pin.PosW);
+
+    float ambient = gAmbientLight * diffuseAlbedo;
+
+    float3 shadowFactor = float3(1.0f, 1.0f, 1.0f);
+    shadowFactor[0] = CalcShadowFactor(pin.ShadowPosH);
+
+    const float shininess = (1.0f - roughness) * normalMapSample.a;
+    Material mat = { diffuseAlbedo, fresnelR0, shininess };
+    float4 directLight = ComputeLighting(
+        gLights, mat, pin.PosW, bumpedNormalW, toEyeW, shadowFactor
+    );
+
+    float4 litColor = ambient + directLight;
+
+    float3 r = reflect(-toEyeW, bumpedNormalW);
+    float4 reflectionColor = gCubeMap.Sample(gsamLinearWrap, r);
+    float3 fresnelFactor = SchlickFresnel(fresnelR0, bumpedNormalW, r);
+    litColor.rgb += shininess * fresnelFactor * reflectionColor.rgb;
+    litColor.a = diffuseAlbedo.a;
+
+    return litColor;
+}
