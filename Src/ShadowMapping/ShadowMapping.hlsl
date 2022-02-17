e2d578b98923237a9effda141b09c2bb1010cf16
Carlos López Garcés <carlos.lopez.garces@gmail.com>
2022-02-16 23:53:55 -0800

    Use TangentU from VertexIn instead of TangentW

--- Src/ShadowMapping/ShadowMapping.hlsl
+++ Src/ShadowMapping/ShadowMapping.hlsl
@@ -36,7 +36,7 @@ VertexOut VS(VertexIn vin) {
     float4 posW = mul(float4(vin.PosL, 1.0f), gWorld);
     vout.PosW = posW.xyz;
     vout.NormalW = mul(vin.NormalL, (float3x3) gWorld);
-    vout.TangentW = mul(vin.TangentW, (float3x3) gWorld);
+    vout.TangentW = mul(vin.TangentU, (float3x3) gWorld);
     vout.PosH = mul(posW, gViewProj);
 
     float4 texC = mul(float4(vin.TexC, 0.0f, 1.0f), gTexTransform);
