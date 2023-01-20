8c5f54e59c34a24dff6b12e955a9ffb37e9b6139
Carlos López Garcés <carlos.lopez.garces@gmail.com>
2023-01-20 13:00:27 -0800

    Add StencilingApp::BuildMainModelGeometry

--- Src/Stenciling/StencilingApp.cpp
+++ Src/Stenciling/StencilingApp.cpp
@@ -402,102 +402,81 @@ void StencilingApp::BuildGeometry() {
 }
 
 void StencilingApp::BuildMainModelGeometry() {
-  std::ifstream fin("Assets/car.txt");
+    std::ifstream fin("Models/skull.txt");
+	
+	if(!fin) {
+		MessageBox(0, L"Models/skull.txt not found.", 0, 0);
+		return;
+	}
 
-  if (!fin) {
-      MessageBox(0, L"Assets/car.txt not found.", 0, 0);
-      return;
-  }
+	UINT vcount = 0;
+	UINT tcount = 0;
+	std::string ignore;
 
-  UINT vcount = 0;
-  UINT tcount = 0;
-  std::string ignore;
+	fin >> ignore >> vcount;
+	fin >> ignore >> tcount;
+	fin >> ignore >> ignore >> ignore >> ignore;
+	
+	std::vector<Vertex> vertices(vcount);
+	for(UINT i = 0; i < vcount; ++i) {
+		fin >> vertices[i].Pos.x >> vertices[i].Pos.y >> vertices[i].Pos.z;
+		fin >> vertices[i].Normal.x >> vertices[i].Normal.y >> vertices[i].Normal.z;
+		vertices[i].TexC = { 0.0f, 0.0f };
+	}
 
-  fin >> ignore >> vcount;
-  fin >> ignore >> tcount;
-  fin >> ignore >> ignore >> ignore >> ignore;
+	fin >> ignore;
+	fin >> ignore;
+	fin >> ignore;
 
-  XMFLOAT3 vMinf3(+Math::Infinity, +Math::Infinity, +Math::Infinity);
-  XMFLOAT3 vMaxf3(-Math::Infinity, -Math::Infinity, -Math::Infinity);
+	std::vector<std::int32_t> indices(3 * tcount);
+	for(UINT i = 0; i < tcount; ++i) {
+		fin >> indices[i*3+0] >> indices[i*3+1] >> indices[i*3+2];
+	}
 
-  XMVECTOR vMin = XMLoadFloat3(&vMinf3);
-  XMVECTOR vMax = XMLoadFloat3(&vMaxf3);
+	fin.close();
 
-  std::vector<Vertex> vertices(vcount);
-  for (UINT i = 0; i < vcount; ++i) {
-    fin >> vertices[i].Pos.x >> vertices[i].Pos.y >> vertices[i].Pos.z;
-    fin >> vertices[i].Normal.x >> vertices[i].Normal.y >> vertices[i].Normal.z;
+	const UINT vbByteSize = (UINT)vertices.size() * sizeof(Vertex);
 
-    vertices[i].TexC = { 0.0f, 0.0f };
+	const UINT ibByteSize = (UINT)indices.size() * sizeof(std::int32_t);
 
-    XMVECTOR P = XMLoadFloat3(&vertices[i].Pos);
+	auto geo = std::make_unique<MeshGeometry>();
+	geo->Name = "skullGeo";
 
-    XMVECTOR N = XMLoadFloat3(&vertices[i].Normal);
+	ThrowIfFailed(D3DCreateBlob(vbByteSize, &geo->VertexBufferCPU));
+	CopyMemory(geo->VertexBufferCPU->GetBufferPointer(), vertices.data(), vbByteSize);
 
-    XMVECTOR up = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
-    if(fabsf(XMVectorGetX(XMVector3Dot(N, up))) < 1.0f - 0.001f) {
-        XMVECTOR T = XMVector3Normalize(XMVector3Cross(up, N));
-        XMStoreFloat3(&vertices[i].TangentU, T);
-    } else {
-        up = XMVectorSet(0.0f, 0.0f, 1.0f, 0.0f);
-        XMVECTOR T = XMVector3Normalize(XMVector3Cross(N, up));
-        XMStoreFloat3(&vertices[i].TangentU, T);
-    }
-    
-    vMin = XMVectorMin(vMin, P);
-    vMax = XMVectorMax(vMax, P);
-  }
+	ThrowIfFailed(D3DCreateBlob(ibByteSize, &geo->IndexBufferCPU));
+	CopyMemory(geo->IndexBufferCPU->GetBufferPointer(), indices.data(), ibByteSize);
 
-  BoundingBox bounds;
-  XMStoreFloat3(&bounds.Center, 0.5f*(vMin + vMax));
-  XMStoreFloat3(&bounds.Extents, 0.5f*(vMax - vMin));
+	geo->VertexBufferGPU = d3dUtil::CreateDefaultBuffer(
+        md3dDevice.Get(),
+		mCommandList.Get(),
+        vertices.data(),
+        vbByteSize,
+        geo->VertexBufferUploader
+    );
 
-  fin >> ignore;
-  fin >> ignore;
-  fin >> ignore;
+	geo->IndexBufferGPU = d3dUtil::CreateDefaultBuffer(
+        md3dDevice.Get(),
+		mCommandList.Get(),
+        indices.data(),
+        ibByteSize,
+        geo->IndexBufferUploader
+    );
 
-  std::vector<std::int32_t> indices(3 * tcount);
-  for (UINT i = 0; i < tcount; ++i) {
-      fin >> indices[i * 3 + 0] >> indices[i * 3 + 1] >> indices[i * 3 + 2];
-  }
+	geo->VertexByteStride = sizeof(Vertex);
+	geo->VertexBufferByteSize = vbByteSize;
+	geo->IndexFormat = DXGI_FORMAT_R32_UINT;
+	geo->IndexBufferByteSize = ibByteSize;
 
-  fin.close();
+	SubmeshGeometry submesh;
+	submesh.IndexCount = (UINT)indices.size();
+	submesh.StartIndexLocation = 0;
+	submesh.BaseVertexLocation = 0;
 
-  const UINT vbByteSize = (UINT)vertices.size() * sizeof(Vertex);
+	geo->DrawArgs["skull"] = submesh;
 
-  const UINT ibByteSize = (UINT)indices.size() * sizeof(std::int32_t);
-
-  auto geo = std::make_unique<MeshGeometry>();
-  geo->Name = "mainModelGeo";
-
-  ThrowIfFailed(D3DCreateBlob(vbByteSize, &geo->VertexBufferCPU));
-  CopyMemory(geo->VertexBufferCPU->GetBufferPointer(), vertices.data(), vbByteSize);
-
-  ThrowIfFailed(D3DCreateBlob(ibByteSize, &geo->IndexBufferCPU));
-  CopyMemory(geo->IndexBufferCPU->GetBufferPointer(), indices.data(), ibByteSize);
-
-  geo->VertexBufferGPU = d3dUtil::CreateDefaultBuffer(
-    md3dDevice.Get(), mCommandList.Get(), vertices.data(), vbByteSize, geo->VertexBufferUploader
-  );
-
-  geo->IndexBufferGPU = d3dUtil::CreateDefaultBuffer(
-    md3dDevice.Get(), mCommandList.Get(), indices.data(), ibByteSize, geo->IndexBufferUploader
-  );
-
-  geo->VertexByteStride = sizeof(Vertex);
-  geo->VertexBufferByteSize = vbByteSize;
-  geo->IndexFormat = DXGI_FORMAT_R32_UINT;
-  geo->IndexBufferByteSize = ibByteSize;
-
-  SubmeshGeometry submesh;
-  submesh.IndexCount = (UINT)indices.size();
-  submesh.StartIndexLocation = 0;
-  submesh.BaseVertexLocation = 0;
-  submesh.Bounds = bounds;
-
-  geo->DrawArgs["mainModel"] = submesh;
-
-  mGeometries[geo->Name] = std::move(geo);
+	mGeometries[geo->Name] = std::move(geo);
 }
 
 void StencilingApp::BuildPSOs() {
