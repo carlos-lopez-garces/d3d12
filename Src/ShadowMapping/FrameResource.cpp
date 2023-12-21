3cfa5ddc02ab486f9edd4e45a3c7465211f1f9d5
Carlos López Garcés <carlos.lopez.garces@gmail.com>
2023-12-20 22:22:58 -0500

    Initialize SSAOCB in FrameResource constructor

--- Src/ShadowMapping/FrameResource.cpp
+++ Src/ShadowMapping/FrameResource.cpp
@@ -10,6 +10,7 @@ FrameResource::FrameResource(ID3D12Device* device, UINT passCount, UINT objectCo
   PassCB = std::make_unique<UploadBuffer<PassConstants>>(device, passCount, true);
 	MaterialBuffer = std::make_unique<UploadBuffer<MaterialData>>(device, materialCount, false);
   ObjectCB = std::make_unique<UploadBuffer<ObjectConstants>>(device, objectCount, true);
+  SSAOCB = std::make_unique<UploadBuffer<SSAOConstants>>(device, 1, true);
 }
 
 FrameResource::~FrameResource() {}
